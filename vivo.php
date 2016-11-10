33b69dd9de338de63ba0639c8d748949ebe70d19
Dietoad <dietoad@gmail.com>
2016-11-10 14:23:39 +0800

    Delete vivo.php

--- vivo.php
+++
@@ -1,6 +0,0 @@
-<?php
-print_r($_POST);
-print_r($_REQUEST);
-
-file_put_contents('vivo.log', json_encode($_POST)."\n",FILE_APPEND);
-file_put_contents('vivo.log', json_encode($_REQUEST)."\n",FILE_APPEND);
